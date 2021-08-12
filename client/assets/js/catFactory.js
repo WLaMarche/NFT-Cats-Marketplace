f2effdd855ea53e138b4973a86ef2a03a25aeff8
Will LaMarche <willlamarche@comcast.net>
2021-08-11 22:32:19 -0700

    Stomach decorations

--- client/assets/js/catFactory.js
+++ client/assets/js/catFactory.js
@@ -83,32 +83,14 @@ function eyeVariation(num) {
 
         case 3:
             normalEyes()
-            $('#eyeShapeCode').html('Cops')
+            $('#eyeShapeCode').html('Chillin')
             eyesType2()
             break
 
         case 4:
             normalEyes()
-            $('#eyeShapeCode').html('Chillin')
-            eyesType3()
-            break
-
-        case 5:
-            normalEyes()
             $('#eyeShapeCode').html('Happy Pills')
-            eyesType4()
-            break
-
-        case 5:
-            normalEyes()
-            $('#eyeShapeCode').html('Chill')
-            eyesType5()
-            break
-
-        case 5:
-            normalEyes()
-            $('#eyeShapeCode').html('Chill')
-            eyesType6()
+            eyesType3()
             break
 
     }
@@ -118,8 +100,26 @@ function decorationVariation(num) {
     $('#dnadecoration').html(num)
     switch (num) {
         case 1:
-            $('#decorationName').html('Basic')
             normaldecoration()
+            $('#vibeCode').html('Default')
+            break
+
+        case 2:
+            normaldecoration()
+            $('#vibeCode').html('Hell Yea')
+            stomach2()
+            break
+
+        case 3:
+            normaldecoration()
+            $('#vibeCode').html('Fuck No')
+            stomach3()
+            break
+
+        case 4:
+            normaldecoration()
+            $('#vibeCode').html('Fuck No')
+            stomach4()
             break
     }
 }
@@ -133,21 +133,66 @@ async function eyesType1() {
 }
 
 async function eyesType2() {
-    await $('.eye').find('span').css('border-right', '10px solid')
-}
-
-async function eyesType3() {
     await $('.eye').find('span').css('border-top', '10px solid')
 }
 
-async function eyesType4() {
+async function eyesType3() {
     await $('.eye').find('span').css('border-bottom', '10px solid')
 }
 
 async function normaldecoration() {
     //Remove all style from other decorations
     //In this way we can also use normalDecoration() to reset the decoration style
-    $('.cat__head-dots').css({ "transform": "rotate(0deg)", "height": "48px", "width": "14px", "top": "1px", "border-radius": "0 0 50% 50%" })
-    $('.cat__head-dots_first').css({ "transform": "rotate(0deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "50% 0 50% 50%" })
-    $('.cat__head-dots_second').css({ "transform": "rotate(0deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "0 50% 50% 50%" })
+    await $('.stomach').css({'left': '20px',
+    'top': '82px',
+    'width': '98px',
+    'height': '40px',
+    'border': '5px solid transparent',
+    'border-radius': '50% 50% 40% 40%',
+    'clip-path': 'none'})
+    //$('.cat__head-dots_first').css({ "transform": "rotate(0deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "50% 0 50% 50%" })
+    //$('.cat__head-dots_second').css({ "transform": "rotate(0deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "0 50% 50% 50%" })
+}
+
+async function stomach2() {
+    //Remove all style from other decorations
+    //In this way we can also use normalDecoration() to reset the decoration style
+    await $('.stomach').css({'left': '20px',
+    'top': '82px',
+    'width': '98px',
+    'height': '40px',
+    'border': '5px solid transparent',
+    //'border-radius': '40% 30% 80% 50%',
+    'clip-path': 'polygon(10% 25%, 35% 25%, 35% 0%, 65% 0%, 65% 25%, 90% 25%, 90% 50%, 65% 50%, 65% 100%, 35% 100%, 35% 50%, 10% 50%'})
+    //$('.cat__head-dots_first').css({ "transform": "rotate(0deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "50% 0 50% 50%" })
+    //$('.cat__head-dots_second').css({ "transform": "rotate(0deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "0 50% 50% 50%" })
+}
+
+async function stomach3() {
+    //Remove all style from other decorations
+    //In this way we can also use normalDecoration() to reset the decoration style
+    await $('.stomach').css({'left': '20px',
+    'top': '82px',
+    'width': '98px',
+    'height': '40px',
+    'border': '5px solid transparent',
+    //'border-radius': '40% 30% 80% 50%',
+    'clip-path': 'polygon(20% 0%, 0% 20%, 30% 50%, 0% 80%, 20% 100%, 50% 70%, 80% 100%, 100% 80%, 70% 50%, 100% 20%, 80% 0%, 50% 30%'})
+    //$('.cat__head-dots_first').css({ "transform": "rotate(0deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "50% 0 50% 50%" })
+    //$('.cat__head-dots_second').css({ "transform": "rotate(0deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "0 50% 50% 50%" })
+}
+
+async function stomach4() {
+    //Remove all style from other decorations
+    //In this way we can also use normalDecoration() to reset the decoration style
+    await $('.stomach').css({'left': '20px',
+    'top': '82px',
+    'width': '98px',
+    'height': '40px',
+    'border': '5px solid transparent',
+    //'border-radius': '40% 30% 80% 50%',
+    'clip-path': 'none',
+    'content': '$'})
+    //$('.cat__head-dots_first').css({ "transform": "rotate(0deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "50% 0 50% 50%" })
+    //$('.cat__head-dots_second').css({ "transform": "rotate(0deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "0 50% 50% 50%" })
 }
